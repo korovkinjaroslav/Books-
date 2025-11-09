6fa0400e5a70c7c9725537973c3446fe0eaf473e
korovkinjaroslav <korovkin.jaroslav@gmail.com>
2025-11-09 10:41:53 +0400

    finished project, TODO: presentation etc

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PyQt6 import uic
 
 
 class MainWindow(QMainWindow):
+    # Главное окно
     def __init__(self):
         super().__init__()
         uic.loadUi('MainWindow_design.ui', self)
@@ -19,6 +20,7 @@ class MainWindow(QMainWindow):
         self.search_button.clicked.connect(self.open_search_book)
         self.readed_button.clicked.connect(self.open_readed)
         self.parameters_search_button.clicked.connect(self.open_parameters_search)
+        self.rec_button.clicked.connect(self.open_recommendations)
 
     def open_readed(self):
         self.readed = ReadedBooksWindow()
@@ -32,11 +34,16 @@ class MainWindow(QMainWindow):
         self.parameters_search = SearchParametersWindow()
         self.parameters_search.show()
 
+    def open_recommendations(self):
+        self.recommendations_window = RecommendationsWindow()
+        self.recommendations_window.show()
+
     def closeEvent(self, a0):
         connect.close()
 
 
 class BookWindow(QWidget):
+    # Окно детального отображения книги
     def __init__(self, book):
         super().__init__()
         self.book = book
@@ -44,21 +51,18 @@ class BookWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        try:
-            self.setWindowTitle('Отбражение книги')
-            self.title.setText(self.book.title())
-            self.title.setWordWrap(True)
-            self.cover_pixmap = QPixmap(self.book.cover())
-            self.cover.setPixmap(self.cover_pixmap)
-            self.cover.setScaledContents(True)
-            self.authors.setText(self.book.authors())
-            self.categories.setText(cursor.execute(f"""SELECT name FROM categories 
-            WHERE id={self.book.category()}""").fetchone()[0])
-            self.year.setText(str(self.book.year()))
-            self.change_cover_button.clicked.connect(self.change_cover)
-            self.rate_button.clicked.connect(self.rate)
-        except Exception as e:
-            self.statusBar().showMessage('Извините, возникла ошибка')
+        self.setWindowTitle('Отбражение книги')
+        self.title.setText(self.book.title())
+        self.title.setWordWrap(True)
+        self.cover_pixmap = QPixmap(self.book.cover())
+        self.cover.setPixmap(self.cover_pixmap)
+        self.cover.setScaledContents(True)
+        self.authors.setText(self.book.authors())
+        self.categories.setText(cursor.execute(f"""SELECT name FROM categories 
+        WHERE id={self.book.category()}""").fetchone()[0])
+        self.year.setText(str(self.book.year()))
+        self.change_cover_button.clicked.connect(self.change_cover)
+        self.rate_button.clicked.connect(self.rate)
 
     def change_cover(self):
         self.new_cover = QFileDialog.getOpenFileName(self, 'Выбрать обложку', '')[0]
@@ -73,7 +77,6 @@ class BookWindow(QWidget):
             self,
             "Оценка книги", "Введите вашу оценку (от 1 до 10)",
             value=1, min=1, max=10, step=1)
-        print(self.ok)
         if self.ok:
             if cursor.execute(f"""SELECT * FROM ratings WHERE id = {self.book.id()}""").fetchone():
                 cursor.execute(f"""UPDATE ratings SET rating = {self.number} WHERE id = {self.book.id()}""")
@@ -81,79 +84,67 @@ class BookWindow(QWidget):
             else:
                 cursor.execute(f"""INSERT INTO ratings(id, rating) VALUES({self.book.id()}, {self.number})""")
                 connect.commit()
-            try:
-                self.ids = cursor.execute("""SELECT * FROM ratings""").fetchall()
-                self.average_ratings = [0, 0, 0, 0]
-                for book in self.ids:
-                    id = book[0]
-                    rate = book[1]
-                    self.book2 = Book(f"""SELECT * FROM books WHERE id={id}""")
-                    if self.book2.category() == self.book.category():
-                        self.average_ratings[0] += 1
-                        self.average_ratings[1] += rate
-                    if self.book2.authors() == self.book.authors():
-                        self.average_ratings[2] += 1
-                        self.average_ratings[3] += rate
+            self.ids = cursor.execute("""SELECT * FROM ratings""").fetchall()
+            self.average_ratings = [0, 0, 0, 0]
+            for book in self.ids:
+                id = book[0]
+                rate = book[1]
+                self.book2 = Book(f"""SELECT * FROM books WHERE id={id}""")
+                if self.book2.category() == self.book.category():
+                    self.average_ratings[0] += 1
+                    self.average_ratings[1] += rate
+                if self.book2.authors() == self.book.authors():
+                    self.average_ratings[2] += 1
+                    self.average_ratings[3] += rate
 
-                id_category = cursor.execute(f"""
-                        SELECT id FROM category_preferences WHERE id = {self.book.category()}""").fetchone()
-                if id_category is None:
-                    cursor.execute(f"""INSERT INTO category_preferences(id, rating)
-                     VALUES({self.book.category()}, {self.average_ratings[1] / self.average_ratings[0]})""")
-                else:
-                    cursor.execute(f"""
+            id_category = cursor.execute(f"""
+                    SELECT id FROM category_preferences WHERE id = {self.book.category()}""").fetchone()
+            if id_category is None:
+                cursor.execute(f"""INSERT INTO category_preferences(id, rating)
+                    VALUES({self.book.category()}, {self.average_ratings[1] / self.average_ratings[0]})""")
+            else:
+                cursor.execute(f"""
                         UPDATE category_preferences SET rating = {self.average_ratings[1] / self.average_ratings[0]}
                         WHERE id = {self.book.category()}""")
-                author = cursor.execute(f'''
+            author = cursor.execute(f'''
                                         SELECT id FROM autor_preferences 
                                         WHERE autor = "{self.book.authors()}"''').fetchone()
-                if author is None:
-                    cursor.execute(f'''INSERT INTO autor_preferences(autor, rating)
+            if author is None:
+                cursor.execute(f'''INSERT INTO autor_preferences(autor, rating)
                                      VALUES("{self.book.authors()}", 
                                     {self.average_ratings[1] / self.average_ratings[0]})''')
-                else:
-                    cursor.execute(f'''
+            else:
+                cursor.execute(f'''
                                         UPDATE autor_preferences SET rating = {self.average_ratings[1] / self.average_ratings[0]}
                                         WHERE autor = "{self.book.authors()}"''')
-                connect.commit()
-            except Exception as e:
-                print(e, 'ERROR')
+            connect.commit()
 
 
 class SearchBookWindow(QWidget):
+    # Окно поиска книги по названию
     def __init__(self):
         super().__init__()
         uic.loadUi('SearchBookWindow_design.ui', self)
         self.initUI()
 
     def initUI(self):
-        try:
-            self.books_layout = BooksLayout(self.qwidget)
-            self.button_font = QFont()
-            self.button_font.setPointSize(12)
-            self.scroll_area.setWidgetResizable(True)
-            self.search_button.clicked.connect(self.search)
-        except Exception as e:
-            self.statusBar().showMessage('Извините, возникла ошибка')
+        self.books_layout = BooksLayout(self.qwidget)
+        self.button_font = QFont()
+        self.button_font.setPointSize(12)
+        self.scroll_area.setWidgetResizable(True)
+        self.search_button.clicked.connect(self.search)
 
     def search(self):
-        try:
-            print(f'''SELECT * FROM books WHERE title LIKE "%{self.title_edit.text()}%"''')
-            res = cursor.execute(f'''SELECT * FROM books WHERE title LIKE "%{self.title_edit.text()}%"''').fetchall()
-            self.books_layout.show_books(res, self)
-        except Exception as e:
-            self.statusBar().showMessage('Извините, возникла ошибка')
-            print(e)
+        res = cursor.execute(f'''SELECT * FROM books WHERE title LIKE "%{self.title_edit.text()}%"''').fetchall()
+        self.books_layout.show_books(res, self)
 
     def open(self):
-        try:
-            self.book = Book(f'''SELECT * FROM books WHERE title="{self.sender().text()}"''')
-            self.book.show()
-        except Exception as e:
-            self.statusBar().showMessage('Извините, возникла ошибка')
+        self.book = Book(f'''SELECT * FROM books WHERE title="{self.sender().text()}"''')
+        self.book.show()
 
 
 class ReadedBooksWindow(QWidget):
+    # Окно для отображения прочитанных книг
     def __init__(self):
         super().__init__()
         uic.loadUi('ReadedWindow_design.ui', self)
@@ -176,6 +167,7 @@ class ReadedBooksWindow(QWidget):
 
 
 class SearchParametersWindow(QWidget):
+    # Окно для поиска по параметрам
     def __init__(self):
         super().__init__()
         uic.loadUi('SearchParametersWindow_design.ui', self)
@@ -208,12 +200,31 @@ class SearchParametersWindow(QWidget):
         self.book.show()
 
 
+class RecommendationsWindow(QWidget):
+    # Окно с рекомендациями
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('RecommendationsWindow_design.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.books_layout = BooksLayout(self.qwidget)
+        self.button_font = QFont()
+        self.button_font.setPointSize(12)
+        self.scroll_area.setWidgetResizable(True)
+        self.books_layout.show_books(
+            Recommender(cursor.execute("""SELECT * FROM books""").fetchall()).recommend(15), self)
+
+    def open(self):
+        self.book = Book(f'''SELECT * FROM books WHERE title="{self.sender().text()}"''')
+        self.book.show()
+
+
 class Book:
+    # Класс для работы с книгами
     def __init__(self, query=None, info=None):
-        print(query, info)
         if query is not None:
             info = cursor.execute(query).fetchone()
-            print(info)
         self.info = {'id': info[0],
                      'title': info[1],
                      'category': info[2],
@@ -223,7 +234,6 @@ class Book:
                      'rating': info[6]}
 
     def show(self):
-        print('creating successful')
         self.window = BookWindow(self)
         self.window.show()
 
@@ -250,32 +260,28 @@ class Book:
 
 
 class BooksLayout(QVBoxLayout):
+    # Класс для отображения кнопок с названиями книг из списка
     def __init__(self, window):
-        try:
-            super().__init__(window)
-        except Exception as e:
-            print(e)
+        super().__init__(window)
 
     def show_books(self, res, window):
-        try:
-            while self.count():
-                item = self.takeAt(0)
-                if item:
-                    widget = item.widget()
-                    if widget:
-                        widget.deleteLater()
-            for info in res:
-                button = QPushButton(info[1])
-                button.setMinimumHeight(50)
-                button.setMaximumWidth(540)
-                button.setFont(window.button_font)
-                button.clicked.connect(window.open)
-                self.addWidget(button)
-        except Exception as e:
-            print(e)
+        while self.count():
+            item = self.takeAt(0)
+            if item:
+                widget = item.widget()
+                if widget:
+                    widget.deleteLater()
+        for info in res:
+            button = QPushButton(info[1])
+            button.setMinimumHeight(50)
+            button.setMaximumWidth(540)
+            button.setFont(window.button_font)
+            button.clicked.connect(window.open)
+            self.addWidget(button)
 
 
 class Recommender:
+    # Класс, выдающий n лучших книг по рейтингу среди данных
     def __init__(self, books):
         self.books = books
 
@@ -301,7 +307,7 @@ class Recommender:
                 self.books[i][-1] = '3.5'
             index *= float(self.books[i][-1]) * 2
             self.books[i].append(index)
-        self.books = sorted(self.books, key=lambda x: x[-1])
+        self.books = sorted(self.books, key=lambda x: -x[-1])
         for i in range(len(self.books)):
             self.books[i] = self.books[i][:-1]
         return self.books[:min(n, len(self.books))]
@@ -310,8 +316,6 @@ class Recommender:
 if __name__ == '__main__':
     connect = sqlite3.connect("DB.sqlite")
     cursor = connect.cursor()
-    print(cursor.execute(f"""SELECT name FROM categories 
-            WHERE id=26""").fetchone()[0])
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
